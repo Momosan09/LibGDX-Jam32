b1a72a7fbd84d5089fac72b63de0930d4cce0b80
Momosan09 <gaelpaezg@gmail.com>
2025-03-15 22:34:29 -0300

    Primer commit

---
+++ core/src/com/mygdx/game/recursos/Render.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.recursos;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class Render {
+
+	
+	public static SpriteBatch batch = new SpriteBatch();
+	
+}
